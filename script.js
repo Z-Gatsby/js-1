5f1e60dfc2e29d3ef249ddb3ad3e0893428e1234
ziyod <8ziyod8@gmail.com>
2022-01-04 17:02:40 +0500

    js-1

---
+++ script.js
@@ -0,0 +1,25 @@
+let name = prompt('Введите своё имя')
+console.log('Ваше имя ' + name);
+
+let age = +prompt('Введите свой возраст')
+console.log('Ваш возраст ' + age);
+
+let plus = +prompt('8 + 2 = ?')
+alert('8 + 2 = ' + (8+2) + ' (Ваш ответ ' + plus + ')');
+console.log('8 + 2 = ' + (8+2) + ' (Ваш ответ ' + plus + ')');
+
+let minus = +prompt('8 - 2 = ?')
+alert('8 - 2 = ' + (8-2) + ' (Ваш ответ ' + minus + ')')
+console.log('8 - 2 = ' + (8-2) + ' (Ваш ответ ' + minus + ')');
+
+let multiplication = +prompt('8 * 2 = ?')
+alert('8 * 2 = ' + (8*2) + ' (Ваш ответ ' + multiplication + ')')
+console.log('8 * 2 = ' + (8*2) + ' (Ваш ответ ' + multiplication + ')');
+
+let division = +prompt('8 / 2 = ?')
+alert('8 / 2 = ' + (8/2) + ' (Ваш ответ ' + division + ')')
+console.log('8 / 2 = ' + (8/2) + ' (Ваш ответ ' + division + ')');
+
+let remainderDivision = +prompt('9 % 2 = ?')
+alert('9 % 2 = ' + (9%2) + ' (Ваш ответ ' + remainderDivision + ')')
+console.log('9 % 2 = ' + (9%2) + ' (Ваш ответ ' + remainderDivision + ')');
